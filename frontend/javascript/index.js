d7bec5d761383f92930b82b824f725fe582ae276
Christopher da Rosa Michel <christopher.michel@cwi.com.br>
2017-10-28 16:21:15 -0200

    feat(Base): criar arquivos base.

---
+++ frontend/javascript/index.js
@@ -0,0 +1,16 @@
+var builds = [];
+
+function gerarBuilds(role) {
+
+    switch(role) {
+        case "Slayer":
+            rolesSlayer();
+            break;
+        case "Marksman":
+            rolesMarksman();
+    }
+}
+
+function rolesSlayer() {
+    
+}
