2e724bf197f0ee44aa4691ac5992edf783d486aa
Unknown <phbattistellavieira@gmail.com>
2017-10-29 00:08:44 -0200

    Auto stash before merge of "master" and "origin/master"

--- frontend/javascript/lista-builds.js
+++ frontend/javascript/lista-builds.js
@@ -2,31 +2,173 @@ var builds =
 [
     {
         Id: 1,
-        role: "Slayer",
+        role: "ADC",
         itens: 
         [
             {
-                nome: exemplo,
-                imagem: exemplo
+                nome: 'Lamina da Fúria de Guinsoo',
+                imagem: null,
+                elo: 'Bronze',
+                tipo: 'AD'
             },
             {
-                nome: outro,
-                imagem: outro
+                nome: 'Furacao de Runaan',
+                imagem: null,
+                elo: 'Bronze',
+                tipo: 'AD'
+            },
+            {
+                nome: 'Grevas do Berserker',
+                imagem: null,
+                elo: 'Bronze',
+                tipo: 'Botas'
+            },
+            {
+                nome: 'Espada do Rei Destruido',
+                imagem: null,
+                elo: 'Prata',
+                tipo: 'AD'
+            },
+            {
+                nome: 'Lembrete Mortal',
+                imagem: null,
+                elo: 'Bronze',
+                tipo: 'AD'
+            },
+            {
+                nome: 'Anjo Guardiao',
+                imagem: null,
+                elo: 'Bronze',
+                tipo: 'AD'
             }
         ]
     },
     {
         Id: 2,
-        role: "Slayer",
+        role: "Assassino",
         itens: 
         [
             {
-                nome: exemplo,
-                imagem: exemplo
+                nome: 'Gume do Infinito',
+                imagem: null,
+                elo: 'Bronze',
+                tipo: 'AD'
             },
             {
-                nome: outro,
-                imagem: outro
+                nome: 'Limiar da Noite',
+                imagem: null,
+                elo: 'Ouro',
+                tipo: 'AD'
+            },
+            {
+                nome: 'Crepusculo de Dartharr',
+                imagem: null,
+                elo: 'Bronze',
+                tipo: 'AD'
+            },
+            {
+                nome: 'Lamina Fantasma de Yomuu',
+                imagem: null,
+                elo: 'Bronze',
+                tipo: 'AD'
+            },
+            {
+                nome: 'Botas da Rapidez',
+                imagem: null,
+                elo: 'Bronze',
+                tipo: 'AD'
+            },
+            {
+                nome: 'Anjo Guardiao',
+                imagem: null,
+                elo: 'Bronze',
+                tipo: 'AD'
+            }
+        ]
+    },
+    {
+        Id: 3,
+        role: "Lutador",
+        itens: 
+        [
+            {
+                nome: 'Lamina da Furia de Guinsoo',
+                imagem: null,
+                elo: 'Bronze',
+                tipo: 'AD'
+            },
+            {
+                nome: 'Semblante Espiritual',
+                imagem: null,
+                elo: 'Bronze',
+                tipo: 'Tanque'
+            },
+            {
+                nome: 'Grevas do Berserker',
+                imagem: null,
+                elo: 'Bronze',
+                tipo: 'Botas'
+            },
+            {
+                nome: 'Espada do Rei Destruido',
+                imagem: null,
+                elo: 'Prata',
+                tipo: 'AD'
+            },
+            {
+                nome: 'Dança da Morte',
+                imagem: null,
+                elo: 'Bronze',
+                tipo: 'AD'
+            },
+            {
+                nome: 'O Cutelo Negro',
+                imagem: null,
+                elo: 'Bronze',
+                tipo: 'AD'
+            }
+        ]
+    },
+    {
+        Id: 4,
+        role: "ADC",
+        itens: 
+        [
+            {
+                nome: 'Coletor de Essencia',
+                imagem: null,
+                elo: 'Bronze',
+                tipo: 'AD'
+            },
+            {
+                nome: 'Dancarina Fantasma',
+                imagem: null,
+                elo: 'Bronze',
+                tipo: 'AD'
+            },
+            {
+                nome: 'Grevas do Berserker',
+                imagem: null,
+                elo: 'Bronze',
+                tipo: 'Botas'
+            },
+            {
+                nome: 'Espada do Rei Destruido',
+                imagem: null,
+                elo: 'Prata',
+                tipo: 'AD'
+            },
+            {
+                nome: 'Lembrancas do Lorde Dominik',
+                imagem: null,
+                elo: 'Bronze',
+                tipo: 'AD'
+            },
+            {
+                nome: 'Mandibula de Malmortius',
+                imagem: null,
+                elo: 'Bronze',
+                tipo: 'AD'
             }
         ]
     }
