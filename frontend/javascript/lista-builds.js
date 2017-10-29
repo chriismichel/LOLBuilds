7259c5c57e51f3f85343f75a420171340ddfba29
Unknown <phbattistellavieira@gmail.com>
2017-10-29 01:13:47 -0200

    imagens adicionadas

--- frontend/javascript/lista-builds.js
+++ frontend/javascript/lista-builds.js
@@ -7,37 +7,37 @@ var builds =
         [
             {
                 nome: 'Lamina da Fúria de Guinsoo',
-                imagem: null,
+                imagem: https://www.mobafire.com/images/item/guinsoos-rageblade.gif,
                 elo: 'Bronze',
                 tipo: 'AD'
             },
             {
                 nome: 'Furacao de Runaan',
-                imagem: null,
+                imagem: https://www.mobafire.com/images/item/runaans-hurricane.gif,
                 elo: 'Bronze',
                 tipo: 'AD'
             },
             {
                 nome: 'Grevas do Berserker',
-                imagem: null,
+                imagem: https://www.mobafire.com/images/item/berserkers-greaves.gif,
                 elo: 'Bronze',
                 tipo: 'Botas'
             },
             {
                 nome: 'Espada do Rei Destruido',
-                imagem: null,
+                imagem: https://www.mobafire.com/images/item/blade-of-the-ruined-king.gif,
                 elo: 'Prata',
                 tipo: 'AD'
             },
             {
                 nome: 'Lembrete Mortal',
-                imagem: null,
+                imagem: https://www.mobafire.com/images/item/mortal-reminder.gif,
                 elo: 'Bronze',
                 tipo: 'AD'
             },
             {
                 nome: 'Anjo Guardiao',
-                imagem: null,
+                imagem: https://www.mobafire.com/images/item/guardian-angel.gif,
                 elo: 'Bronze',
                 tipo: 'AD'
             }
@@ -50,37 +50,37 @@ var builds =
         [
             {
                 nome: 'Gume do Infinito',
-                imagem: null,
+                imagem: https://www.mobafire.com/images/item/infinity-edge.gif,
                 elo: 'Bronze',
                 tipo: 'AD'
             },
             {
                 nome: 'Limiar da Noite',
-                imagem: null,
+                imagem: https://www.mobafire.com/images/item/edge-of-night.gif,
                 elo: 'Ouro',
                 tipo: 'AD'
             },
             {
                 nome: 'Crepusculo de Dartharr',
-                imagem: null,
+                imagem: https://www.mobafire.com/images/item/duskblade-of-draktharr.gif,
                 elo: 'Bronze',
                 tipo: 'AD'
             },
             {
                 nome: 'Lamina Fantasma de Yomuu',
-                imagem: null,
+                imagem: https://www.mobafire.com/images/item/youmuus-ghostblade.gif,
                 elo: 'Bronze',
                 tipo: 'AD'
             },
             {
                 nome: 'Botas da Rapidez',
-                imagem: null,
+                imagem: https://www.mobafire.com/images/item/boots-of-swiftness.gif,
                 elo: 'Bronze',
                 tipo: 'AD'
             },
             {
                 nome: 'Anjo Guardiao',
-                imagem: null,
+                imagem: https://www.mobafire.com/images/item/guardian-angel.gif,
                 elo: 'Bronze',
                 tipo: 'AD'
             }
@@ -93,37 +93,37 @@ var builds =
         [
             {
                 nome: 'Lamina da Furia de Guinsoo',
-                imagem: null,
+                imagem: https://www.mobafire.com/images/item/guinsoos-rageblade.gif,
                 elo: 'Bronze',
                 tipo: 'AD'
             },
             {
                 nome: 'Semblante Espiritual',
-                imagem: null,
+                imagem: https://www.mobafire.com/images/item/spirit-visage.gif,
                 elo: 'Bronze',
                 tipo: 'Tanque'
             },
             {
                 nome: 'Grevas do Berserker',
-                imagem: null,
+                imagem: https://www.mobafire.com/images/item/berserkers-greaves.gif,
                 elo: 'Bronze',
                 tipo: 'Botas'
             },
             {
                 nome: 'Espada do Rei Destruido',
-                imagem: null,
+                imagem: https://www.mobafire.com/images/item/blade-of-the-ruined-king.gif,
                 elo: 'Prata',
                 tipo: 'AD'
             },
             {
                 nome: 'Dança da Morte',
-                imagem: null,
+                imagem: https://www.mobafire.com/images/item/deaths-dance.gif,
                 elo: 'Bronze',
                 tipo: 'AD'
             },
             {
                 nome: 'O Cutelo Negro',
-                imagem: null,
+                imagem: https://www.mobafire.com/images/item/the-black-cleaver.gif,
                 elo: 'Bronze',
                 tipo: 'AD'
             }
@@ -136,37 +136,37 @@ var builds =
         [
             {
                 nome: 'Coletor de Essencia',
-                imagem: null,
+                imagem: https://www.mobafire.com/images/item/essence-reaver.gif,
                 elo: 'Bronze',
                 tipo: 'AD'
             },
             {
                 nome: 'Dancarina Fantasma',
-                imagem: null,
+                imagem: https://www.mobafire.com/images/item/phantom-dancer.gif,
                 elo: 'Bronze',
                 tipo: 'AD'
             },
             {
                 nome: 'Grevas do Berserker',
-                imagem: null,
+                imagem: https://www.mobafire.com/images/item/berserkers-greaves.gif,
                 elo: 'Bronze',
                 tipo: 'Botas'
             },
             {
                 nome: 'Espada do Rei Destruido',
-                imagem: null,
+                imagem: https://www.mobafire.com/images/item/blade-of-the-ruined-king.gif,
                 elo: 'Prata',
                 tipo: 'AD'
             },
             {
                 nome: 'Lembrancas do Lorde Dominik',
-                imagem: null,
+                imagem: https://www.mobafire.com/images/item/lord-dominiks-regards.gif,
                 elo: 'Bronze',
                 tipo: 'AD'
             },
             {
                 nome: 'Mandibula de Malmortius',
-                imagem: null,
+                imagem: https://www.mobafire.com/images/item/maw-of-malmortius.gif,
                 elo: 'Bronze',
                 tipo: 'AD'
             }
